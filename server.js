62c62809cfccbcf2ba2a42b04b83bcf12bd6848c
Johan Lundqvist <jolundq@gmail.com>
2026-05-23 19:03:40 +0200

    wip [skip ci]

--- server.js
+++ server.js
@@ -71,15 +71,13 @@ http.createServer((req, res) => {
       </table>`;
   }
 
-  const buildInfo = `
-    <hr style="margin-top:40px">
-    <p style="color:#888;font-size:0.8em">
+  const buildInfo = `<p style="color:#888;font-size:0.8em;margin-top:2px">
       commit: ${process.env.GIT_COMMIT || 'unknown'} &nbsp;|&nbsp;
       built: ${process.env.BUILD_TIME || 'unknown'}
     </p>`;
 
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end(`<html><body><h1>hello from basic-app</h1>${authSection}${certSection}${configSection()}${buildInfo}</body></html>\n`);
+  res.end(`<html><body><h1>hello from basic-app</h1>${buildInfo}${authSection}${certSection}${configSection()}</body></html>\n`);
 }).listen(PORT, () => {
   console.log(`app on http://localhost:${PORT}`);
 });
